5567c6dd22daea6dfc3c0c05696c9607a35713cf
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2020-05-11 13:27:05 +0530

    Data class added

---
+++ src/Data.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Data
+{
+    static Map<String,String> numsToBinary = new HashMap<>();
+    static Map<String,String> binaryToNums = new HashMap<>();
+
+    static {
+        binaryToNums.put("0000","0");
+        binaryToNums.put("0001","1");
+        binaryToNums.put("0010","2");
+        binaryToNums.put("0011","3");
+        binaryToNums.put("0100","4");
+        binaryToNums.put("0101","5");
+        binaryToNums.put("0110","6");
+        binaryToNums.put("0111","7");
+        binaryToNums.put("1000","8");
+        binaryToNums.put("1001","9");
+        binaryToNums.put("1010","A");
+        binaryToNums.put("1011","B");
+        binaryToNums.put("1100","C");
+        binaryToNums.put("1101","D");
+        binaryToNums.put("1110","E");
+        binaryToNums.put("1111","F");
+
+
+        numsToBinary.put("0","0000");
+        numsToBinary.put("1","0001");
+        numsToBinary.put("2","0010");
+        numsToBinary.put("3","0011");
+        numsToBinary.put("4","0100");
+        numsToBinary.put("5","0101");
+        numsToBinary.put("6","0110");
+        numsToBinary.put("7","0111");
+        numsToBinary.put("8","1000");
+        numsToBinary.put("9","1001");
+        numsToBinary.put("A","1010");
+        numsToBinary.put("B","1011");
+        numsToBinary.put("C","1100");
+        numsToBinary.put("D","1101");
+        numsToBinary.put("E","1110");
+        numsToBinary.put("F","1111");
+    }
+}
--- src/OnePassAssembler.java
+++ src/OnePassAssembler.java
@@ -26,54 +26,11 @@ public class OnePassAssembler
     private static  Map<String,String> opcodes = new HashMap<>();
     private static Map<String,String> objectCodes = new HashMap<>();
     private static Map<String,String> SymTab = new HashMap<>();
-
-    private static Map<String,String> numsToBinary = new HashMap<>();
-    private static Map<String,String> binaryToNums = new HashMap<>();
-
     private static List<references> forwardReferences = new ArrayList<>();
-
-    private static void initializeMaps()
-    {
-        binaryToNums.put("0000","0");
-        binaryToNums.put("0001","1");
-        binaryToNums.put("0010","2");
-        binaryToNums.put("0011","3");
-        binaryToNums.put("0100","4");
-        binaryToNums.put("0101","5");
-        binaryToNums.put("0110","6");
-        binaryToNums.put("0111","7");
-        binaryToNums.put("1000","8");
-        binaryToNums.put("1001","9");
-        binaryToNums.put("1010","A");
-        binaryToNums.put("1011","B");
-        binaryToNums.put("1100","C");
-        binaryToNums.put("1101","D");
-        binaryToNums.put("1110","E");
-        binaryToNums.put("1111","F");
-
-
-        numsToBinary.put("0","0000");
-        numsToBinary.put("1","0001");
-        numsToBinary.put("2","0010");
-        numsToBinary.put("3","0011");
-        numsToBinary.put("4","0100");
-        numsToBinary.put("5","0101");
-        numsToBinary.put("6","0110");
-        numsToBinary.put("7","0111");
-        numsToBinary.put("8","1000");
-        numsToBinary.put("9","1001");
-        numsToBinary.put("A","1010");
-        numsToBinary.put("B","1011");
-        numsToBinary.put("C","1100");
-        numsToBinary.put("D","1101");
-        numsToBinary.put("E","1110");
-        numsToBinary.put("F","1111");
-    }
 
 
     public static void main(String[] args) throws Exception
     {
-        initializeMaps();
 
         File inputProgramFile = new File("D:\\InputProgam.txt");
         File opcodeFile  = new File("D:\\opcode.txt");
@@ -139,14 +96,14 @@ public class OnePassAssembler
                 {
                     String opnum = opcodes.get(opcode);
 
-                    String ObjectCode = numsToBinary.get(opnum.toCharArray()[0]+"")+ numsToBinary.get(opnum.toCharArray()[1]+"");
+                    String ObjectCode = Data.numsToBinary.get(opnum.toCharArray()[0]+"")+ Data.numsToBinary.get(opnum.toCharArray()[1]+"");
                     //System.out.println(ObjectCode);
 
                     ObjectCode += "110010";
 
                     ObjectCode = ObjectCode.substring(0,6)+ObjectCode.substring(8);
 
-                    String miniObj = binaryToNums.get(ObjectCode.substring(0,4))+binaryToNums.get(ObjectCode.substring(4,8))+binaryToNums.get(ObjectCode.substring(8));
+                    String miniObj = Data.binaryToNums.get(ObjectCode.substring(0,4))+Data.binaryToNums.get(ObjectCode.substring(4,8))+Data.binaryToNums.get(ObjectCode.substring(8));
                     //System.out.println(miniObj);
 
                     System.out.println(Integer.toHexString(nextAdd)+":\t\t"+opcode+"\t\t"+operand);
@@ -206,14 +163,14 @@ public class OnePassAssembler
 
                         String opnum = opcodes.get(opcode);
 
-                        String ObjectCode = numsToBinary.get(opnum.toCharArray()[0]+"")+ numsToBinary.get(opnum.toCharArray()[1]+"");
+                        String ObjectCode = Data.numsToBinary.get(opnum.toCharArray()[0]+"")+ Data.numsToBinary.get(opnum.toCharArray()[1]+"");
                         //System.out.println(ObjectCode);
 
                         ObjectCode += "110010";
 
                         ObjectCode = ObjectCode.substring(0,6)+ObjectCode.substring(8);
 
-                        String miniObj = binaryToNums.get(ObjectCode.substring(0,4))+binaryToNums.get(ObjectCode.substring(4,8))+binaryToNums.get(ObjectCode.substring(8));
+                        String miniObj = Data.binaryToNums.get(ObjectCode.substring(0,4))+Data.binaryToNums.get(ObjectCode.substring(4,8))+Data.binaryToNums.get(ObjectCode.substring(8));
                         //System.out.println(miniObj);
 
                         //System.out.println(nextAdd+":\t\t"+opcode+"\t\t"+operand);
